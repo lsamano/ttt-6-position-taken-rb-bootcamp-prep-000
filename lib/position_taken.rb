2577c77d118d8c9aa6b72a2e182ab3331153a598
Leizl Samano <leizl.samano@gmail.com>
2018-11-19 17:58:46 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+# code your #position_taken? method here!
+def position_taken?( board, index )
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    #false
+    puts "Hi."
+  else
+    # true
+    puts "Yo."
+end
